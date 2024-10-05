5fe94d183ed141d2f397d2bc5b6b6f8b62a9efd7
dmi <david.mignot@compart.com>
2024-10-05 14:55:07 +0200

    hotfilx_multi users

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes setup
 app.use('/', indexRouter);
+app.use('/users', userRoutes);  // Make sure this is included in your routes setup
 app.use('/user', userRoutes);  // Use the /user route for anything in userRoutes
 
 app.use((req, res, next) => {
